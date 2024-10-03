52e6b18c603022df23318625d2551ae3af1886b6
chorus <galuscad05@gmail.com>
2024-10-03 22:51:27 +0300

    Customizing the project and dependencies

--- ZayShop/Program.cs
+++ ZayShop/Program.cs
@@ -1,6 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Добавляем поддержку контроллеров и представлений
+builder.Services.AddControllersWithViews();
+
+// Настраиваем контекст базы данных и подключаем PostgreSQL
+builder.Services.AddDbContext<OrderContext>(options =>
+options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Включаем перенаправление на HTTPS
+app.UseHttpsRedirection();
+
+// Подключаем статические файлы
+app.UseStaticFiles();
+
+// Настраиваем маршрутизацию
+app.UseRouting();
+
+// Подключаем авторизацию (если есть)
+app.UseAuthorization();
+
+// Настраиваем маршрут по умолчанию
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
